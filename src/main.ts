3e6748027918a928647ea854d2ec04764fbc2e9b
Nunong21 <nuno@moloni.com>
2020-06-12 16:59:39 +0100

    Auto

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,36 @@ import { getInput, setOutput, setFailed, info } from '@actions/core';
 import { context } from '@actions/github';
 
 import { composerVersion } from './composer';
+import { tagVersion } from './tag';
 
 try {
+  let currentVersion = '';
+
   const usedMethod = getInput('method');
   const filePath = getInput('path');
 
   info(`Creating a new version using method: ${usedMethod}`);
   info(`Using the directory: ${filePath}`);
 
-  const nextVersion = 'V1.0.1';
+
 
   if (usedMethod === 'composer.json') {
-    setOutput("version", composerVersion(filePath));
+    currentVersion = composerVersion(filePath);
   } else {
-
+    currentVersion = tagVersion();
   }
 
+  if (currentVersion.length === 0) {
+    setFailed("Failed obtaining the current version");
+  } else {
+    info(`Next Version: ${currentVersion}`);
+    setOutput("version", currentVersion);
 
-  if (context.payload && context.payload.head_commit && context.payload.head_commit.message) {
-    setOutput("commit_title", context.payload.head_commit.message);
+    if (context.payload && context.payload.head_commit && context.payload.head_commit.message) {
+      setOutput("commit_title", context.payload.head_commit.message);
+    }
   }
 
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
   setFailed('This is a test that should fail');
 } catch (error) {
   setFailed(error.message);
--- src/tag.ts
+++ src/tag.ts
@@ -5,20 +5,16 @@ export const tagVersion = (): string => {
     if (err) {
       console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
       console.log('\x1b[31m%s\x1b[0m', stderr);
-      process.exit(1);
-
       return '';
     }
 
     exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
       if (err) {
+        console.log(`On rev ${rev}`);
         console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
         console.log('\x1b[31m%s\x1b[0m', stderr);
-        process.exit(1);
-
         return '';
       }
-
 
       console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
 
